0e34f78e05226c91277ef42328f80c4c2ab2f07b
unknown <amaury_interdemilan@hotmail.com>
2019-09-05 17:16:16 -0500

    Practica 4 en clase

--- 04-ModeladoGeometrico/src/main.cpp
+++ 04-ModeladoGeometrico/src/main.cpp
@@ -15,11 +15,11 @@
 #include "Headers/Shader.h"
 
 // Model geometric includes
-/*
 #include "Headers/Sphere.h"
 #include "Headers/Cylinder.h"
 #include "Headers/Box.h"
-*/
+#include "Headers/FirstPersonCamera.h"
+
 
 //GLM include
 #define GLM_FORCE_RADIANS
@@ -31,16 +31,18 @@ int screenWidth;
 int screenHeight;
 
 GLFWwindow * window;
-
+std::shared_ptr<FirstPersonCamera> camera(new FirstPersonCamera());
 Shader shader;
 
-/*Sphere sphere1(20, 20);
+Sphere sphere1(20, 20);
+Sphere sphere2(20, 20);
 Cylinder cylinder1(20, 20, 0.5, 0.5);
-Box box1;*/
+Box box1;
 
 bool exitApp = false;
-int lastMousePosX;
-int lastMousePosY;
+int lastMousePosX, offsetX = 0;
+int lastMousePosY, offsetY = 0;
+
 
 double deltaTime;
 
@@ -108,17 +110,31 @@ void init(int width, int height, std::string strTitle, bool bFullScreen) {
 
 	shader.initialize("../Shaders/colorShader.vs", "../Shaders/colorShader.fs");
 
-	/*sphere1.init();
+	// INICIALIZAR LOS BUFFERS VAO, VBO, EBO
+	sphere1.init();
+	// MÉTODO SETTER QUE COLOCA EL APUNTADOR AL SHADER QUE VAMOS A  OCUPAR. 
 	sphere1.setShader(&shader);
+	//sETTER PARA PONER EL COLOR DE LA GEOMETRÍA
 	sphere1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));
 
+	// INICIALIZAR LOS BUFFERS VAO, VBO, EBO ESFERA 2
+	sphere2.init();
+	// MÉTODO SETTER QUE COLOCA EL APUNTADOR AL SHADER QUE VAMOS A  OCUPAR. 
+	sphere2.setShader(&shader);
+	//sETTER PARA PONER EL COLOR DE LA GEOMETRÍA
+	sphere2.setColor(glm::vec4(0.2, 1.3, 1.0, 1.0));
+
+
+
+	//
 	cylinder1.init();
 	cylinder1.setShader(&shader);
 	cylinder1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));
 
 	box1.init();
 	box1.setShader(&shader);
-	box1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));*/
+	box1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));
+	camera->setPosition(glm::vec3(2.0, 0.0, 4.0));
 }
 
 void destroy() {
@@ -127,9 +143,9 @@ void destroy() {
 	// --------- IMPORTANTE ----------
 	// Eliminar los shader y buffers creados.
 
-	/*sphere1.destroy();
-	cylinder1.destroy();
-	box1.destroy();*/
+	sphere1.destroy();
+	cylinder1.destroy(); //MANDA A EJECUTAR TODOS LOS DESTRUCTORES DE LOS VAO, EBO Y VBO
+	box1.destroy();		//ESTO PARA LIBERAR LA MEMORIA
 
 	shader.destroy();
 }
@@ -151,8 +167,12 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
 }
 
 void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+	offsetX = xpos - lastMousePosX;
+	offsetY = ypos - lastMousePosY;
 	lastMousePosX = xpos;
 	lastMousePosY = ypos;
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+		camera->mouseMoveCamera(offsetX, offsetY, deltaTime);
 }
 
 void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
@@ -180,6 +200,17 @@ bool processInput(bool continueApplication){
 	TimeManager::Instance().CalculateFrameRate(false);
 	deltaTime = TimeManager::Instance().DeltaTime;
 
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) //NOS AYUDAN PARA SI PRESIONAMOS LA TECLA W AVANCEMOS
+		camera->moveFrontCamera(true, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) //NOS AYUDAN PARA SI PRESIONAMOS LA TECLA S RETROCEDER
+		camera->moveFrontCamera(false, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) //NOS AYUDAN PARA SI PRESIONAMOS LA TECLA A IZQ
+		camera->moveRightCamera(false, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) //NOS AYUDAN PARA SI PRESIONAMOS LA TECLA D DER
+		camera->moveRightCamera(true, deltaTime);
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+		camera->mouseMoveCamera(offsetX, offsetY, 0.0001);
+
 	glfwPollEvents();
 	return continueApplication;
 }
@@ -191,7 +222,8 @@ void applicationLoop() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) screenWidth / (float) screenHeight, 0.01f, 100.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+		glm::mat4 view = camera->getViewMatrix(); //glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+
 
 		shader.turnOn();
 
@@ -200,14 +232,50 @@ void applicationLoop() {
 
 		glm::mat4 model = glm::mat4(1.0f);
 
-		/*sphere1.render(model);
-		sphere1.enableWireMode();*/
 
+		/*sphere1.enableWireMode(); //VISUALIZAR CON LINEAS LA ESFERA
+		//HACE EL DIBUJADO DE LA GEOMETRÍA Y RECIBE LA MATRIZ DE TRANSFORMACIÓN
+		sphere1.render(model); 
 		/*cylinder1.render(model);
 		cylinder1.enableWireMode();*/
+		
+		//CAJITA------------------
+		box1.enableWireMode();
+		box1.render(scale(model, glm::vec3(1.0, 1.0, 0.1)));
+		//----------------------
+		//ARTICULACIÓN 1-----------------------------------------------
+		glm::mat4 j1 = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
+		sphere1.enableWireMode();
+		sphere1.render(glm::scale(j1, glm::vec3(0.1, 0.1, 0.1)));
+		//--------------------------------------------------------------------
+		//HUESO1
+		glm::mat4 L1 = glm::translate(j1, glm::vec3(0.25, 0.0, 0.0));
+		L1 = glm::rotate(L1, glm::radians(90.0f), glm::vec3(0, 0, 1.0));
+		cylinder1.enableWireMode();
+		cylinder1.render(glm::scale(L1, glm::vec3(0.1, 0.5, 0.1)));
+		//--------------------------------------------------------------------
+		//ARTICULACIÓN 2
+		glm::mat4 j2 = glm::translate(j1, glm::vec3(0.25, 0.0f, 0.0f)); //j2 va a depender de j1
+		sphere1.enableWireMode();
+		sphere1.render(glm::scale(j2, glm::vec3(0.1, 0.1, 0.1)));
+		//------------------------------------------------------
+		//OJO dDERECHO
+		glm::mat4 ojo = glm::translate(model, glm::vec3(0.25, 0.25, 0.05f)); //ojo se va a mover con el modelo, depende del modelo.
+		sphere1.enableWireMode();						//HACE QUE SE VEA CON LINEAS
+		sphere1.render(glm::scale(ojo, glm::vec3(0.2, 0.2, 0.1)));	//MUESTRA L FIGURA PERO LA DEBE ESCALAR CON SCALE
+		//OJO IZQUIERDO------------------------
+		glm::mat4 ojo2 = glm::translate(model, glm::vec3(-0.25, 0.25, 0.05f)); //ojo se va a mover con el modelo, depende del modelo.
+		sphere2.enableWireMode();						//HACE QUE SE VEA CON LINEAS
+		sphere2.render(glm::scale(ojo2, glm::vec3(0.2, 0.2, 0.1)));	//MUESTRA L FIGURA PERO LA DEBE ESCALAR CON SCALE
 
-		/*box1.render(model);
-		box1.enableWireMode();*/
+
+		//HUSEO 2-----------
+		/*glm::mat4 j2 = glm::translate(j2, glm::vec3(0.25, 0.0, 0.0));
+		l2 = glm::rotate(l2, glm::radians(90.0f), glm::vec3(0, 0, 1.0));
+		cylinder1.enableFillMode();
+		cylinder1.render(l2);
+		*/
+
 
 		shader.turnOff();
 
