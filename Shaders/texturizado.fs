9e01a08c82e7013b75f18892e5779eacbfdc444b
unknown <amaury_interdemilan@hotmail.com>
2019-09-26 15:20:48 -0500

    None

--- Shaders/texturizado.fs
+++ Shaders/texturizado.fs
@@ -1,11 +1,11 @@
 #version 330 core
 
-//in vec2 out_uv;
+in vec2 out_uv;
 out vec4 color;
-//uniform sampler2D ourTexture;
+uniform sampler2D ourTexture; //unidad de textura de tipo sampler2D 
 
 void main(){
 	
-	//color = texture(ourTexture, out_uv);
-	color = vec4(0.3, 0.5, 0.4, 1.0);
+	color = texture(ourTexture, out_uv); //Obtiene el color de la textura en base a la unidad de la textura y las coordenadas 
+	//color = vec4(0.3, 0.5, 0.4, 1.0);
 }
